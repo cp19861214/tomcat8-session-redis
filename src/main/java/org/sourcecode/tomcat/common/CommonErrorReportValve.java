e40a7c47731cb1df169f711f345b86a9a97afddb
chenpeng <chenpeng@co188.com>
2016-07-17 17:39:00 +0800

    修改错误

--- src/main/java/org/sourcecode/tomcat/common/CommonErrorReportValve.java
+++ src/main/java/org/sourcecode/tomcat/common/CommonErrorReportValve.java
@@ -12,13 +12,15 @@ public class CommonErrorReportValve extends ErrorReportValve {
 
 	protected Log log = LogFactory.getLog(getClass());
 
+	private String errorPage = "/error.html";
+
 	@Override
 	protected void report(Request request, Response response, Throwable throwable) {
 		String stackTrace = getPartialServletStackTrace(throwable);
 		int statusCode = response.getStatus();
 		try {
-			log.error("statusCode = " + statusCode + ",exception = " + stackTrace);
-			response.sendError(statusCode, "System is Occur a Error ...");
+			log.error("statusCode = " + statusCode + ",exception:\r\n" + stackTrace);
+			response.sendRedirect(errorPage + "?msg=System is Occur a Error", statusCode);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
