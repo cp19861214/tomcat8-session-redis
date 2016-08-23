2c95790461d946048323d5aed43b8f7e739d8242
chenpeng <chenpeng@co188.com>
2016-08-23 21:19:57 +0800

    update num

--- src/main/java/org/sourcecode/redis/client/DefaultJedisPoolConfig.java
+++ src/main/java/org/sourcecode/redis/client/DefaultJedisPoolConfig.java
@@ -9,7 +9,7 @@ public class DefaultJedisPoolConfig extends JedisPoolConfig {
 		setTestWhileIdle(true);
 		setMinEvictableIdleTimeMillis(60000);// 超过一分钟的被回收
 		setTimeBetweenEvictionRunsMillis(30000);// 30秒检查一次
-		setNumTestsPerEvictionRun(-1);// 每次回收的个数配置
+		setNumTestsPerEvictionRun(3);// 每次回收的个数配置
 		setMaxTotal(300);// 最大连接数
 		setMaxIdle(10);// 最大空闲数
 		setMinIdle(1);// 最小空闲数
